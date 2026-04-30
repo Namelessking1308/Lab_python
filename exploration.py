cdf25c64494706dd36951718181e7e49da7e9c6f
Grégory Cuyle <gcuyle@gmail.com>
2026-04-30 16:12:07 +0200

    moveRoom DisplayRoom ok

--- exploration.py
+++ exploration.py
@@ -8,19 +8,33 @@ def createRooms() :
     
     rooms = [
         {"name": "Salle du Temps", "desc": "Cette salle est reliée à une autre dimension où le temps s'écoule différemment. Une minute en dehors de cette salle équivaut à 6 heures dedans, et un jour à une année. Cette salle est utilisée par les combatants pour augmenter leurs potentiels", "exit" : "nord"},
-        {"name": "Salle de l'Esprit", "desc": "super salle", "exit" : "sud"},
-        {"name": "Salle de la Force", "desc": "super salle", "exit" : "est"},
-        {"name": "Salle de l'Âme", "desc": "super salle", "exit" : "ouest"}
+        {"name": "Salle de l'Esprit", "desc": "Ici, les grands esprits se rencontrent pour une bataille mentale", "exit" : "sud"},
+        {"name": "Salle de la Force", "desc": "Ici la gravité est 10x supérieure à la normale. Cette salle est utilisée pour augmenter leur force", "exit" : "est"},
+        {"name": "Salle de l'Âme", "desc": "Si votre âme est pure, vous survivrez à cette salle. Sinon, malédiction à vous.", "exit" : "ouest"}
     ]
 
+# Change current room
+def moveRoom(currentRoom, direction) :
 
-def move(currentRoom) :
-    print()
+    if(direction == rooms[currentRoom]["exit"]) :
+        print(f"Vous vous dirigez dans la direction {direction}. Vous voyez la sortie et sortez de la salle.")
 
+        newRoom = random.randint(0, 3)
+        while( newRoom == currentRoom):
+            newRoom = random.randint(0, 3)
+    else :
+        print("Vous ne voyez pas la sortie ...")
+    
+
+# Display room details
 def displayRoom(number) : 
-    print(f"Bienvenue dans la salle numéro {number+1}")
-    for i in range (rooms[number]["name"].len()) :
-        print("=")
-    print(rooms[number]["name"])
-    for i in range (rooms[number]["name"].len()) :
-        print("=")
+    #Welcome
+    print(f"Bienvenue dans la salle numéro {number+1} !\n")
+    #Name
+    for i in range (len(rooms[number]["name"])) :
+        print("=",end="")
+    print("\n"+rooms[number]["name"])
+    for i in range (len(rooms[number]["name"])) :
+        print("=",end="")
+    #Desc
+    print("\n\nDescription : "+rooms[number]["desc"])
